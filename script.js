d42e921351bb0ddb0f523211b1c23ab18ca29460
kavyarajannna <140047089+kavyarajannna@users.noreply.github.com>
2025-03-27 17:45:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+    // Toggle hamburger menu visibility
+    const hamburger = document.getElementById('hamburger');
+    const navbar = document.querySelector('.navbar');
+    const navLinks = document.querySelector('.nav-links');
+
+    // Open and close hamburger menu
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+        hamburger.classList.toggle('close');
+    });
+
+    // Close hamburger when clicking outside
+    document.addEventListener('click', (event) => {
+        if (!hamburger.contains(event.target) && !navbar.contains(event.target)) {
+            navLinks.classList.remove('active');
+            hamburger.classList.remove('close');
+        }
+    });
+
+    const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', (e) => {
+        e.stopPropagation();
+        const dropdownMenu = dropdown.querySelector('.dropdown-menu');
+        dropdownMenu.classList.toggle('active');
+    });
+});
